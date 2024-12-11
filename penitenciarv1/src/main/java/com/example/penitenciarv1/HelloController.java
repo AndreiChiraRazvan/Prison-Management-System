a78cd22cb0b6f933ff2f383c04a04a8bf4c659a6
AndreiChira <chiraandrei222@gmail.com>
2024-12-11 18:43:23 +0200

    Login fail Final

--- penitenciarv1/src/main/java/com/example/penitenciarv1/HelloController.java
+++ penitenciarv1/src/main/java/com/example/penitenciarv1/HelloController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
@@ -45,10 +46,12 @@ public class HelloController {
 
     @FXML
     public void failed_login_btn_onClick(ActionEvent e) throws IOException {
+        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
         Stage returnToLogin = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource(("hello-view.fxml")));
         returnToLogin.setScene(new Scene(root));
         returnToLogin.show();
+        stage.close();
     }
 
     @FXML
