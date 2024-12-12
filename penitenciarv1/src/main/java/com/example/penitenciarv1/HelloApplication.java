0c09aaa95b932c7c548048d7a67d05d31306bc84
JustDragos <53474641+JustDragos@users.noreply.github.com>
2024-12-12 14:18:04 +0200

    Detalii detinuti (#6)
    * Incercam
    
    * Interfetetot (#5)
    
    * Poza cu inchisoare
    
    * Interfata gardian final
    
    * Interfete
    
    ---------
    
    Co-authored-by: paulgiurgiu <pgiurgiu194@gmail.com>
    
    * Incercam
    
    * Incercam
    
    ---------
    
    Co-authored-by: dragos <dragospeter18@gmail.com>
    Co-authored-by: paulgiurgiu <pgiurgiu194@gmail.com>

--- penitenciarv1/src/main/java/com/example/penitenciarv1/HelloApplication.java
+++ penitenciarv1/src/main/java/com/example/penitenciarv1/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -18,35 +19,30 @@ public class HelloApplication extends Application {
         try {
 
 
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("interfatagardian.fxml"));
-
-            // Use an absolute path to load the FXML file
-//            String fxmlPath = "./hello-view.fxml";
-//            File fxmlFile = new File(fxmlPath);
-//            URL fxmlUrl = fxmlFile.toURI().toURL();
-          
             Parent root = fxmlLoader.load();
 
-            primaryStage.setTitle("Gardian-Meniu principal");
-//            Scene scene = new Scene(root);
-//            primaryStage.setScene(scene);
-//            primaryStage.show();
-//          StackPane root2 = new StackPane();
-            root.setId("pane");
-            Scene scene2 = new Scene(root, 600, 450);
-            scene2.getStylesheets().addAll(this.getClass().getResource("demo.css").toExternalForm());
-            primaryStage.setScene(scene2);
-            //pentru setare minim si maxim
-            primaryStage.setMinWidth(500);
-            primaryStage.setMinHeight(500);
-
+            primaryStage.setTitle("LogIn");
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
             primaryStage.show();
+            StackPane root2 = new StackPane();
+            root.setId("pane");
+
 
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Error loading FXML file. Ensure the file path is correct and the file exists.");
         }
+    }
+    private void changeBackground(Scene scene, String color) {
+        scene.getRoot().setStyle(String.format(
+                "-fx-background-color: %s;" +
+                        "-fx-background-position: center; " +
+                        "-fx-background-repeat: no-repeat;",
+                color
+        ));
     }
 
     public static void main(String[] args) {
