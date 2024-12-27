1e5ca0a10bb89f6189baa413e1e1085cb5c902b0
JustDragos <53474641+JustDragos@users.noreply.github.com>
2024-12-27 21:35:45 +0200

    Adaugat procedura vazut sentinta totala
    Ca sa functioneze, trebuie adaugata procedura in workbench

---
+++ procedures/procedure_remaining_sentence_time_based_on_id_detinut.sql
@@ -0,0 +1,12 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `remaining_time_based_on_id_inmate`(IN idPrizonier int, out sentinta_ramasa nvarchar(155))
+BEGIN
+	set sentinta_ramasa = (select (if(Sum(TIMESTAMPDIFF(month, sentinta.start_time, sentinta.end_time)) > 2,
+		Concat(Sum(TIMESTAMPDIFF(month, sentinta.start_time, sentinta.end_time)), 
+            ' months') ,
+		if ( Sum(TIMESTAMPDIFF(day, sentinta.start_time, sentinta.end_time)) > 1, 
+        Concat(Sum(TIMESTAMPDIFF(day, sentinta.start_time, sentinta.end_time)), ' days')
+        , Concat(Sum(TIMESTAMPDIFF(hour, sentinta.start_time, sentinta.end_time)), ' hour'))
+        ))
+	from sentinta where sentinta.fk_id_detinut = idPrizonier);
+														
+END
