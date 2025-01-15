e1c8c34facbdad830a742cf38320b2dcec8f9a27
paulgiurgiu <pgiurgiu194@gmail.com>
2025-01-15 23:09:20 +0200

    W

--- penitenciarv1/src/main/java/com/example/penitenciarv1/Listeners/DynamicScallingAppPrisonerVisit.java
+++ penitenciarv1/src/main/java/com/example/penitenciarv1/Listeners/DynamicScallingAppPrisonerVisit.java
@@ -129,7 +129,7 @@ public class DynamicScallingAppPrisonerVisit extends Application {
         DatabaseConnector dbConnector = new DatabaseConnector();
         try (Statement statement = dbConnector.conn.createStatement()) {
        //     String query = "CALL GetVisitorScheduleByUsername('\" + detinutUsername + \"')";
-            String query = "CALL GetVisitorScheduleByUsernamePerfUPDATED1('" + detinutUsername + "')";
+            String query = "CALL GetVisitorScheduleByUsernamePerfUPDATED1234('" + detinutUsername + "')";
 
 
             ResultSet resultSet = statement.executeQuery(query);
